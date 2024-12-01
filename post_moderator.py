b914df37bbbc97b97444d334e39d161f99e4b931
William Cheung <william.cheung@pb.com>
2024-12-01 17:34:39 -0500

    Refactor code for get_category_from_pronpt_file, get_prompt_files from post_moderator.py to utils.py.

--- post_moderator.py
+++ post_moderator.py
@@ -1,15 +1,11 @@
-import os
-
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from langchain.prompts import PromptTemplate
 from typing import Callable, Generator
 
 from llm import invoke_llm, invoke_llm2
-from utils import timeit
+from utils import PROMPTS_DIR, get_category_from_pronpt_file, get_prompt_files, timeit
 
-PROMPTS_DIR = 'prompts'
-PROMPT_FILE_EXT = '.PROMPT.txt'
-prompt_files = [file_name for file_name in os.listdir(PROMPTS_DIR) if file_name.endswith(PROMPT_FILE_EXT)]
+prompt_files = get_prompt_files()
 
 def moderate_post(post: str) -> str|Generator[str, None, None]:
     print(f'Post: {post}')
@@ -18,7 +14,7 @@ def moderate_post(post: str) -> str|Generator[str, None, None]:
     with ThreadPoolExecutor(max_workers=2) as executor: # process 2 prompts at a time, using llm and llm2 in parallel
         futures = []
         for i, prompt_file in enumerate(prompt_files):
-            category = prompt_file.replace(PROMPT_FILE_EXT, '')
+            category = get_category_from_pronpt_file(prompt_file)
             with open(f'{PROMPTS_DIR}/{prompt_file}', 'r', encoding='utf-8') as f:
                 prompt = f.read()
             prompt = f"Context: {category}\n{prompt}\nThe negative label should be used ONLY in the *DIRECT* context of the above. Do NOT label as negative based on a different context than the above, even if related. If the post is not EXPLICITLY related to the context, do NOT use negative. You are not certified to make judgements on other contexts."
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import decorator
+import os
 import time
+
+PROMPTS_DIR = 'prompts'
+PROMPT_FILE_EXT = '.PROMPT.txt'
+
+def get_prompt_files() -> list[str]:
+    return [file_name for file_name in os.listdir(PROMPTS_DIR) if file_name.endswith(PROMPT_FILE_EXT)]
+
+def get_category_from_pronpt_file(prompt_file: str) -> str:
+    return prompt_file.replace(PROMPT_FILE_EXT, '')
 
 @decorator.decorator
 def timeit(func, *args, **kwargs):
